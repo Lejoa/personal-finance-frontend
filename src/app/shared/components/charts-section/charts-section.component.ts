6372fd709c669c83f4e66d562aea1d0d1b824d89
Alejandro Gomez Anaya <alejandro.gomez@correounivalle.edu.co>
2025-10-25 20:36:31 -0500

    date-picker component was added and configured with Angular Material

--- src/app/shared/components/charts-section/charts-section.component.ts
+++ src/app/shared/components/charts-section/charts-section.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { PieChartComponent } from '../pie-chart/pie-chart.component';
 
 @Component({
   selector: 'app-charts-section',
   standalone: true,
   imports: [
-    PieChartComponent
+    PieChartComponent,
+    MatFormFieldModule, 
+    MatDatepickerModule, 
+    FormsModule, 
+    ReactiveFormsModule,
   ],
   templateUrl: './charts-section.component.html',
   styleUrl: './charts-section.component.scss'
 })
 export class ChartsSectionComponent {
-
+  readonly today = new Date();
+  
+  readonly defaultStartDate = new Date(this.today.getFullYear(), this.today.getMonth() - 1, 1);
+  readonly defaultEndDate = new Date(this.today.getFullYear(), this.today.getMonth(), 0);
+  
+  readonly range = new FormGroup({
+    start: new FormControl<Date>(this.defaultStartDate),
+    end: new FormControl<Date>(this.defaultEndDate),
+  });
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+// src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideNativeDateAdapter } from '@angular/material/core'; // ✅ Agregar DateAdapter
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideCharts(withDefaultRegisterables())],
+  providers: [
+    ...appConfig.providers,
+    provideAnimations(),
+    provideNativeDateAdapter(), // Adapter for Angular Material
+    provideCharts(withDefaultRegisterables())
+  ]
 }).catch((err) => console.error(err));
