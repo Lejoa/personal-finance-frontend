d12505e47f16ccb5805c71cfa8fa3c6891cd9ec1
Alejandro Gomez Anaya <alejandro.gomez@correounivalle.edu.co>
2026-05-10 17:38:10 -0500

    refactor: angluar.json and karma.conf.js files were modified to support unit tests coverage in bash

--- karma.conf.js
+++ karma.conf.js
@@ -1,34 +1,35 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['mocha'],
-    files: [
-      'src/**/*.spec.ts'
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-    exclude: [],
-    preprocessors: {},
-    reporters: ['progress'],
+    client: {
+      jasmine: {},
+      clearContext: false
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['ChromeHeadlessCI'],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--disable-gpu',
-          '--disable-dev-shm-usage',
-          '--disable-setuid-sandbox',
-          '--disable-software-rasterizer'
-        ]
-      }
-    },
-    plugins: [
-      'karma-mocha',
-      'karma-chrome-launcher'
-    ]
+    restartOnFileChange: false
   });
 };
